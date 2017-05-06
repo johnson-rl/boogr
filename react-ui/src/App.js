88de829f6480cc32845713e0fcfd8d988e87e3fd
monzelb <monzelb@gmail.com>
2017-05-06 15:32:16 -0700

    nothing'

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
 
-      <div className="App">
-        <Game />
+      <div className= "App ">
+        <Test />
       </div>
     );
   }
--- react-ui/src/test.js
+++ react-ui/src/test.js
@@ -4,7 +4,7 @@ const Test = (props) => {
   console.log(props)
 
     return(
-      <div>
+      <div className="col-xs-6 game">
         <img src={"https://i.imgur.com/DFzuiNJ.png"} alt="sub" className="banana img-responsive"/>
       </div>
       )
