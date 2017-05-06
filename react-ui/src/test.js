126d03d2336e639a4ef1e968bb965bcc0457e268
monzelb <monzelb@gmail.com>
2017-05-06 15:19:42 -0700

    added submarine component

---
+++ react-ui/src/test.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+const Test = (props) => {
+  console.log(props)
+
+    return(
+      <div>
+        <img src={"https://i.imgur.com/DFzuiNJ.png"} alt="sub" className="banana img-responsive"/>
+      </div>
+      )
+}
+
+export default Test
