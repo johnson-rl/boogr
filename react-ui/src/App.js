2900ca572430ecc2ec86dbec7e1a5b9371b21c30
monzelb <monzelb@gmail.com>
2017-05-06 16:25:50 -0700

    banana

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
 
       <div className="App">
-        <Game />
+        <Test />
       </div>
     );
   }
--- react-ui/src/test.js
+++ react-ui/src/test.js
@@ -4,8 +4,23 @@ const Test = (props) => {
   console.log(props)
 
     return(
-      <div>
-        <img src={"https://i.imgur.com/DFzuiNJ.png"} alt="sub" className="banana img-responsive"/>
+      <div className="col-xs-6 game">
+        <div className="col-xs-2 tissue-div">
+          <img src={"https://i.imgur.com/qIRjoAW.jpg"} alt="sub" className="tissue-img img-responsive"/>
+        </div>
+        <div className="col-xs-3 tissue-count">
+          <h4>Tissue Count:</h4>
+        </div>
+        <div className="col-xs-3 col-xs-offset-2 tissue-count">
+          <h4>Tissue Count:</h4>
+        </div>
+        
+        <div className="col-xs-2 booger-div">
+          <img src={"https://i.imgur.com/ntqBYhK.jpg"} alt="sub" className="booger-img img-responsive"/>
+        </div>
+        <div className="sub">
+          <img src={"https://i.imgur.com/LX0IkHs.jpg"} alt="sub" className="banana-img img-responsive"/>
+        </div>
       </div>
       )
 }
