c0f36a97035a67e6716d155386faf62dcaaa11d1
Dmitry Akhchin <dmitryakhchin@Dmitrys-MacBook-Pro.local>
2017-05-07 16:03:24 -0700

    added audio to correct / wrong answer. added little submarine to game board

--- react-ui/src/board.js
+++ react-ui/src/board.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Board extends Component {
   render() {
     return(
-      <div>
+      <div className="gameBoard">
         <img src="https://www.dropbox.com/s/ag5znfkn8bshzfi/Map.png?dl=1" className="img-responsive board" alt="game board" />
+
+        <img src="https://www.dropbox.com/s/vzrl59rr59nllkq/boat.png?dl=1" width="70px" className="progress-boat" />
       </div>
     );
   }
