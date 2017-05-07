a19565c09bc638b317a088723a6587344fb05b82
monzelb <monzelb@gmail.com>
2017-05-06 22:52:24 -0700

    boogers

--- react-ui/src/enemy.js
+++ react-ui/src/enemy.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 class Enemy extends Component {
   render() {
     return(
-      <div>
-        <img src="https://www.dropbox.com/s/35u721enx6fyf8h/booger.png?dl=1" className="img-responsive enemy" alt="booooooogers" />
+      <div className="boogs">
+        <div className="col-xs-3 col-xs-offset-2">
+          <img src="https://www.dropbox.com/s/35u721enx6fyf8h/booger.png?dl=1" className="img-responsive enemy1" alt="booooooogers" />
+        </div>
+        <div className="col-xs-3">
+          <img src="https://www.dropbox.com/s/35u721enx6fyf8h/booger.png?dl=1" className="img-responsive enemy1" alt="booooooogers" />
+        </div>
+        <div className="col-xs-5">
+          <img src="https://www.dropbox.com/s/35u721enx6fyf8h/booger.png?dl=1" className="img-responsive enemy1" alt="booooooogers" />
+        </div>
+        <div className="col-xs-6">
+          <img src="https://www.dropbox.com/s/35u721enx6fyf8h/booger.png?dl=1" className="img-responsive enemy1" alt="booooooogers" />
+        </div>
       </div>
     );
   }
 }
 
 export default Enemy;
+
