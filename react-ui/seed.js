9fa3edc6356d1f14700edeef4711a05cd0560ed4
monzelb <monzelb@gmail.com>
2017-05-07 15:08:32 -0700

    added seed file

--- react-ui/seed.js
+++ react-ui/seed.js
@@ -1,4 +1,5 @@
 [
+//2nd grade Science
 {
   question: "What do you call a skinny booger?",
   correct: "Slim Pickins.",
@@ -72,7 +73,7 @@
 {
   question: "Which weighs more?",
   correct: "Five pounds of rocks",
-  incorrect: [“One pound of feathers”],
+  incorrect: ["One pound of feathers"],
   difficulty: 1,
   subject: "Science"
 },
@@ -128,7 +129,7 @@
 {
   question: "Why does your body digest food?",
   correct: "For energy",
-  incorrect: [“For sleep”, “For watching TV”, “For your hair”],
+  incorrect: ["For sleep", "For watching TV", "For your hair"],
   difficulty: 1
   subject: "Science"
 },
@@ -168,6 +169,275 @@
   subject: "Science"
 },
 {
+  question: "The main purpose of boogers is:",
+  correct: "To keep bad things out of your body.",
+  incorrect: ["To be gross!", "To make you sweat.", "To answer these questions!"],
+  difficulty: 1,
+  subject: "Science"
+},
+
+
+//3rd Grade science
+{
+  question: "During which stage does a caterpillar become a chrysalis?",
+  correct: "Pupa",
+  incorrect: ["Egg", "Larva", "Adult"],
+  difficulty: 2,
+  subject: "Science"
+},
+{
+  question: "An organism with no eyes, no legs, no mouth, and and wings might be a:",
+  correct: "Plant",
+  incorrect: ["Worm", "Snake", "Butterfly"],
+  difficulty: 2,
+  subject: "Science"
+},
+
+{
+  question: " Humans breath in oxygen, what gas do they breath out? ",
+  correct: "B. CO2",
+  incorrect: ["A. Oxygen.","C. Helium.", "D. HO2SO4."],
+  difficulty: 2,
+  subject: "Science"
+},
+{
+  question: "What is a scientist who studies the body called?",
+  correct: "Physiologist",
+  incorrect: ["Veterinarian", "Biologist", "Meteorologist"],
+  difficulty: 2,
+  subject: "Science"
+},
+{
+  question: "What is connected to your bones to help them move?",
+  correct: "Muscles",
+  incorrect: ["Brain", "Blood", "Other Bones"],
+  difficulty: 2,
+  subject: "Science"
+},
+{
+  question: "What provides information to your brain about external conditions?",
+  correct: "Nerve Receptors",
+  incorrect: ["Heart", "Bones", "Blood Vessels"],
+  difficulty: 2,
+  subject: "Science"
+},
+{
+  question: "What is the scientific or medical name for the skull?",
+  correct: "The Cranium",
+  incorrect: ["The Femur", "The Gluteus Maximus", "Humerus"],
+  difficulty: 2,
+  subject: "Science"
+},
+
+
+
+
+//2nd grade math
+{
+  question: "9 - 7 = ?",
+  correct: "2",
+  incorrect: ["1", "3", "16"],
+  difficulty: 1,
+  subject: "Math"
+},
+{
+  question: "20 - 5 = ?",
+  correct: "15",
+  incorrect: ["0", "4", "5"],
+  difficulty: 1,
+  subject: "Math"
+},
+{
+  question: "8 - 3 = ?",
+  correct: "5",
+  incorrect: ["2", "3", "4"],
+  difficulty: 1,
+  subject: "Math"
+},
+{
+  question: "12 - 4 = ?",
+  correct: "8",
+  incorrect: ["4", "6", "16"],
+  difficulty: 1,
+  subject: "Math"
+},
+{
+  question: "4 - 0 = ?",
+  correct: "4",
+  incorrect: ["2", "0", "8"],
+  difficulty: 1,
+  subject: "Math"
+},
+{
+  question: "9 - 9 = ?",
+  correct: "0",
+  incorrect: ["9", "18", "20"],
+  difficulty: 1,
+  subject: "Math"
+},
+
+
+//3rd grade Math
+{
+  question: "Which addition problem is the same as 525+379?",
+  correct: "500 + 300 + 20 + 70 + 5 + 9",
+  incorrect: ["50 + 30 + 20 + 70 + 50 + 90", "500 + 300 + 20 + 90 + 5 + 7", "250 + 250 + 300 + 30 + 60 + 20"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "Which addition problem is the same as 45 + 917?",
+  correct: "9 hundreds + 4 tens + 1 ten + 5 ones + 7 ones",
+  incorrect: ["7 hundreds + 9 tens + 5 tens + 1 ten + 7 ones", "9 hundreds + 4 hundreds + 1 ten + 5 tens + 7 ones", "9 hundreds + 5 tens + 1 ten + 4 ones + 7 ones"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "Which expression could be used to find the value of 871 + 140?",
+  correct: "8 hundreds + 1 hundred + 7 tens + 4 tens + 1 one",
+  incorrect: ["8 hundreds + 1 hundred + 7 tens + 4 ones + 1 one", "8 ones + 1 one + 7 ones + 4 ones + 1 one", "8 tens + 1 hundred + 7 ones + 4 tens + 1 one"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "93 + 45 is the same as:",
+  correct: "90 + 47",
+  incorrect: ["80 + 35", "85 + 33", "95 + 23"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "Which addition problem is the same as 350 + 473?",
+  correct: "50 + 300 + 400 + 30 + 40 + 3",
+  incorrect: ["30 + 50 + 40 + 70 + 40 + 3", "300 + 70 + 400 + 500 + 40 + 3", "70 + 30 + 50 + 300 + 40 + 3"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "Which addition problem is the same as 23 + 501?",
+  correct: "5 hundreds + 2 tens + 3 ones + 1 one",
+  incorrect: ["2 hundreds + 5 tens + 3 ones + 1 one", "2 tens + 5 ones + 3 hundreds + 1 one", "1 hundred + 5 ones + 2 tens + 1 one"]
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "What is the product of 2 and 70?",
+  correct: "140",
+  incorrect: ["72", "104", "270"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "Sanders ran for 22 minutes. Gabe ran 7 minutes longer than Sanders did. Helen ran 12 minutes less than Gabe. For how many minutes did Helen run?",
+  correct: "17 minutes",
+  incorrect: ["3 minutes", "27 minutes", "41 minutes"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "What is 342 - 184?",
+  correct: "158",
+  incorrect: ["162", "242", "268"],
+  difficulty: 2,
+  subject: "Math"
+},
+{
+  question: "Fill in the blank: 56 + __ = 7",
+  correct: "8 days",
+  incorrect: ["27 days", "49 days", "63 days"],
+  difficulty: 2,
+  subject: "Math"
+},
+
+// 4th Grade Math
+{
+  question: "128 / 8",,
+  correct: "16",
+  incorrect: ["17", "24", "8"],
+  difficulty: 3,
+  subject: "Math"
+},
+{
+  question: "60 * 5",
+  correct: "120",
+  incorrect: ["-120", "130", "90"],
+  difficulty: 3,
+  subject: "Math"
+},
+{
+  question: "81 / 3 ",
+  correct: "27",
+  incorrect: ["9", "13", "20"],
+  difficulty: 3,
+  subject: "Math"
+},
+{
+  question: "100 * 10",
+  correct: "1000",
+  incorrect: ["100", "-100", "10000"],
+  difficulty: 3,
+  subject: "Math"
+},
+{
+  question: "90 / 30",
+  correct: "3",
+  incorrect: ["4", "8", "12"],
+  difficulty: 3,
+  subject: "Math"
+},
+{
+  question: "225 / -15",
+  correct: "-15",
+  incorrect: ["15", "-16", "17"],
+  difficulty: 3,
+  subject: "Math"
+},
+{
+  question: "400 / -20",
+  correct: "20",
+  incorrect: [ "-20", "21", "-12"],
+  difficulty: 3,
+  subject: "Math"
+},
+
+//4th Grade Science
+{
+  question: "What term best describes an electrical charge formed on the outside of an object?",
+  correct: "Static Electricity",
+  incorrect: ["Insulation", "Conduction", "Magnetic Field"],
+  difficulty: 3,
+  subject: "Science"
+},
+{
+  question: "Which property of light breaks white light into the separate colors of the rainbow?",
+  correct: "Refraction",
+  incorrect: ["Reflection", "Light Waves", "Absorption"],
+  difficulty: 3,
+  subject: "Science"
+},
+{
+  question: "Which circuit had two or more paths for a current to flow through?",
+  correct: "Parallel Circuit",
+  incorrect: ["Battery Circuit", "Series Circuit", "Magnetic Circuit"],
+  difficulty: 3,
+  subject: "Science"
+},
+{
+  question: "What materials are attracted to magnets?",
+  correct: "Iron",
+  incorrect: ["Copper", "Aluminum", "Wood"],
+  difficulty: 3,
+  subject: "Science"
+},
+{
+  question: "How much blood is in the average human body? ",
+  correct: "A. About 10 pints. ",
+  incorrect: ["B. About 2 pints.","C. 21 pints.","D. No pints, who needs pints?"],
+  difficulty: 3,
+  subject: "Science"
+},
+
+{
   question: "What is the hollow space in the middle of a bone filled with?",
   correct: "Marrow",
   incorrect: ["Air", "blood", "solid bone"],
@@ -203,38 +473,13 @@
   subject: "Science"
 },
 {
-  question: "During which stage does a caterpillar become a chrysalis?",
-  correct: "Pupa",
-  incorrect: ["Egg", "Larva", "Adult"],
-  difficulty: 2,
-  subject: "Science"
-},
-{
-  question: "An organism with no eyes, no legs, no mouth, and and wings might be a:",
-  correct: "Plant",
-  incorrect: ["Worm", "Snake", "Butterfly"],
-  difficulty: 2,
-  subject: "Science"
-},
-{
-  question: "How much blood is in the average human body? ",
-  correct: "A. About 10 pints. ",
-  incorrect: ["B. About 2 pints.","C. 21 pints.","D. No pints, who needs pints?"],
+  question: "What helps the red blood cells carry oxygen through out the body?",
+  correct: "C. Hemoglobin.",
+  incorrect: ["A. Bubbles","B. Globetrotters.", "D. Chloroplast. "],
   difficulty: 3,
   subject: "Science"
 },
-{
-  question: " Humans breath in oxygen, what gas do they breath out? ",
-  correct: "B. CO2",
-  incorrect: ["A. Oxygen.","C. Helium.", "D. HO2SO4."],
-  difficulty: 2,
-  subject: "Science"
-},
-{
-  question: ,
-  correct: ,
-  incorrect: ,
-  difficulty: 2,
-  subject: "Science"
-},
+
+
+
 ]
